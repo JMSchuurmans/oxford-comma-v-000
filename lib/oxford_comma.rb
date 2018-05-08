7e3d5bdabdfd4f6789aa819364b09d22f74f05b4
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-08 21:17:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    oxford_end_of_array = "and #{array[-1]}"
+    array.pop
+    array.push(oxford_end_of_array)
+    return array.join(", ")
+  end
 end
+
